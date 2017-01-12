6f33dc340e331ae67a1e5e64484cfd878a1b2af1
heavenward <heavenward@bluuprint.com>
2017-01-12 13:31:11 +0800

    add error status

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
-#include "debug.h"
+#include "Debug.h"
+#include "Errors.h"
 
 DEBUG_SET_LEVEL(DEBUG_LEVEL_ERROR);
 int main(void){
-
+status_t t=NO_ERROR;
 	int i = 10;
 	ERROR("i= %d",i);
-	ASSERT("xxxx");
+	ASSERT("error_status:%d",t);
 	return 0;
 }
