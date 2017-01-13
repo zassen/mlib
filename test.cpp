ed027a94cb23cbcdd8a120876c71c716bdb9de3f
heavenward <heavenward@bluuprint.com>
2017-01-13 19:50:06 +0800

    Mutex Condition ready

--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
 #include "Debug.h"
-#include "Errors.h"
+#include "Condition.h"
+#include "Mutex.h"
 
 DEBUG_SET_LEVEL(DEBUG_LEVEL_ERROR);
+using namespace std;
+using namespace mlib;
 int main(void){
 status_t t=NO_ERROR;
+	Mutex mlock;
 	int i = 10;
 	ERROR("i= %d",i);
 	ASSERT("error_status:%d",t);
