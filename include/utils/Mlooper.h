b6256d6a2a08c12f6035bbc027b36fdab268c4b5
heavenward <heavenward@bluuprint.com>
2017-01-23 19:13:55 +0800

    modify Mlooper.h

--- include/utils/Mlooper.h
+++ include/utils/Mlooper.h
@@ -5,18 +5,19 @@
 #include <utils/KeyedVector.h>
 #include <utils/Timers.h>
 #include <string>
+#include <sys/epoll.h>
 using namespace std;
 namespace mlib{
 
 
-
+typedef int (*Mlooper_callbackFunc)(int fd, int events, void *datea);
 
 struct Message{
 
 	Message():mWhat(0){}
 	Message(int what):mWhat(what) {}
 	Message(string str):mStr(str){}
-void 	setData(uint8_t *data,int len){
+	void 	setData(uint8_t *data,int len){
 		memset(&mData,0,128);
 		if(data != NULL){
 
@@ -39,6 +40,45 @@ int MessageTest(const Message &msg)
 	DEBUG("msg.what=%d",msg.mWhat);
 
 	
+
+}
+
+
+class Mlooper {
+
+	protected:
+		virtual ~Mlooper();
+	public:
+		MLooper();
+		int pollOnce(int timeoutMillis, int* outFd, int* outEvents, void** outData);
+		inline int pollOnce(int timeoutMillis){
+			return pollOnce(timeoutMillis, NULL, NULL, NULL);
+		}
+
+		int pollAll(int timeoutMillis, int* outFd, int* outEvents, void** outData);
+		inline int pollAll(int timeoutMillis){
+			return pollAll(timeoutMillis, NULL, NULL, NULL);
+		}
+
+		void wake();
+		int addFd(int fd, int ident, int events, Mlooper_callbackFunc callback, void* data);
+		int removeFd(int fd);
+		bool isIdling() const;
+
+		static Mlooper* prepare(int opts);
+
+		static void setForThread(const Mlooper* mlooper);
+
+		static Mlooper* getForThread();
+	private:
+		int mWakeReadPipeFd;
+	        int mWakeWritePipeFd;	
+		Mutex mlock;
+
+		int pollInner(int timeoutMillis);
+		void awoken();
+
+
 
 }
 
