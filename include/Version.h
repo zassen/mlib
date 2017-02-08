bd1faf715244884b84558b62c44364bd4fb3d6ee
heavenward <heavenward@bluuprint.com>
2017-02-08 23:34:18 +0800

    add always args

--- include/Version.h
+++ include/Version.h
@@ -5,9 +5,9 @@
 namespace mlib{
 
 using namespace std;
-#define GIT_TAG "v0.1-2-g540bb51-dirty" 
-#define GIT_DATE "Wed Feb 8 23:23:08 2017" 
-#define GIT_COMMIT_SUBJECT "modify the version tag" 
+#define GIT_TAG "v0.1-3-gd70c109-dirty" 
+#define GIT_DATE "Wed Feb 8 23:31:16 2017" 
+#define GIT_COMMIT_SUBJECT "correct the version variable tag" 
 
 	static const string mlibVersionTag= GIT_TAG;
 	static const string mlibVersionDate= GIT_DATE;
